7dcce61da53601d11eb4a6024635d252904a2b52
fabien pipard <fabienpipard@gmail.com>
2020-01-29 21:56:45 +0100

    Add special character in test.csv. Update the spec tests.

--- exercice1/spec/datatable_spec.rb
+++ exercice1/spec/datatable_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe JungleTest::DataTable do
   let(:headers) { %i[col1 col2 col3] }
 
   it 'checks the csv conversion to datatable' do
-    expect(datatable).to match_array([{ col1: 'val', col2: '2', col3: 'AAAA' },
+    expect(datatable).to match_array([{ col1: 'val', col2: '2', col3: 'éééé' },
                                       { col1: 'res', col2: '3', col3: 'BBBB' }])
   end
 
